b650d85a98c6b38c1633ff586cf22b6557e6ded8
Ekaterina <reshetuyak@gmail.com>
2022-12-04 23:50:30 +0700

    first add

---
+++ Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Text_quest_Lab
+{
+    class Game
+    {
+        public Game()
+        {
+            // Инициализация всех компонентов игры
+            return;
+        }
+        
+    }
+}
