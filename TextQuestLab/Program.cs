94b8a5ed3beac4e41fc53bb740fdd2ec723dc59f
Asus <Asus@DESKTOP-KTQA9RU>
2022-12-05 11:36:07 +0700

    Добавьте файлы проекта.

---
+++ TextQuestLab/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Text_quest_Lab
+{
+    class Progra
+    {
+        static void Main(string[] args)
+        {
+            TextAnalyzer an = new TextAnalyzer();
+            GameDesk desk = new GameDesk();
+            desk.printQuestions(an.analyze());
+        }
+    }
+}
